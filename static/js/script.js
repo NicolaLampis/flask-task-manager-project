9284a11a6a6eb5c877381aca1d1f694cbb72d90a
NicolaLampis <nico.lampo@gmail.com>
2021-03-19 00:23:38 +0000

    add_task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,11 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
-    $('.tooltipped').tooltip();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd, mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {done: "Select"}
+    });
   });
